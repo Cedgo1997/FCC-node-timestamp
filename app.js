4725c23ad6c29047b45aaa5752633374b691fb27
César Gonzalez <cedgo1997@gmail.com>
2020-12-06 23:42:03 -0400

    first endpoint api/timestamp

--- app.js
+++ app.js
@@ -1,7 +1,14 @@
 // Express
 const express = require('express');
 const app = express();
-// Port
-const port = 3000;
 
+app.get('/api/timestamp', (req, res) => {
+	res.json({
+		unix: new Date().getTime(),
+		utc: new Date().toUTCString(),
+	});
+});
+
+// Port and listen
+const port = 3000;
 app.listen(port, () => console.log('Listening on port: ' + port));
