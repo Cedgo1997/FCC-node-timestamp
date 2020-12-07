1ceb9db7381b40f061b4fe7ec5c1f7ef53eab266
César Gonzalez <cedgo1997@gmail.com>
2020-12-06 23:53:26 -0400

    Second endpoint

--- app.js
+++ app.js
@@ -2,11 +2,37 @@
 const express = require('express');
 const app = express();
 
+//First Timestamp Endpoint
 app.get('/api/timestamp', (req, res) => {
 	res.json({
 		unix: new Date().getTime(),
 		utc: new Date().toUTCString(),
 	});
+});
+
+//Second Timestamp Endpoint
+app.get('/api/timestamp/:date', (req, res) => {
+	let dateParam = req.params.date;
+	let dateConverted = new Date(dateParam);
+	let unixRegex = /\d{5,}/;
+
+	if (unixRegex.test(dateParam)) {
+		res.json({
+			unix: parseInt(dateParam),
+			utc: new Date(parseInt(dateParam)).toUTCString(),
+		});
+	}
+
+	if (dateConverted.toString() === 'Invalid Date') {
+		res.json({
+			error: 'Invalid Date',
+		});
+	} else {
+		res.json({
+			unix: dateConverted.getTime(),
+			utc: dateConverted.toUTCString(),
+		});
+	}
 });
 
 // Port and listen
